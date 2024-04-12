be04dfc1c499a417cc5ecaf22fe9a75ff88385e2
Paulo Henrique Barbosa De Lima <ph.blima30@gmail.comm>
2024-04-12 00:19:36 -0300

    rotas get + post , chamando a resposta do controoler.

---
+++ app/controllers/momentsController.ts
@@ -0,0 +1,10 @@
+/* eslint-disable unicorn/filename-case */
+// import type { HttpContext } from '@adonisjs/core/http'
+
+export default class MomentsController {
+  async store() {
+    return {
+      msg: 'Deu Certo !',
+    }
+  }
+}
