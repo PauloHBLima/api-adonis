e5f83068e0452353a4cb047b1d6941b62997b949
Paulo Henrique Barbosa De Lima <ph.blima30@gmail.comm>
2024-04-12 00:20:22 -0300

    controller

--- start/routes.ts
+++ start/routes.ts
@@ -7,12 +7,17 @@
 |
 */
 
-import Route from '@adonisjs/core/services/router'
+const MomentsController = () => import('#controllers/MomentsController')
+import router from '@adonisjs/core/services/router'
 
-Route.group(() => {
-  Route.get('/', async () => {
-    return {
-      hello: 'one piece exist !',
-    }
+router
+  .group(() => {
+    router.get('/', async () => {
+      return {
+        hello: 'one piece exist !',
+      }
+    })
+
+    router.post('/moments', [MomentsController, 'store'])
   })
-}).prefix('/api')
+  .prefix('/api')
