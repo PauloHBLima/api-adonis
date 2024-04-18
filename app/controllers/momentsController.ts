10e667de2fd8a5410708a32c42beec9b88aa9b6f
Paulo Henrique Barbosa De Lima <ph.blima30@gmail.comm>
2024-04-17 21:52:38 -0300

    create moment

--- app/controllers/momentsController.ts
+++ app/controllers/momentsController.ts
@@ -1,10 +1,11 @@
-/* eslint-disable unicorn/filename-case */
-// import type { HttpContext } from '@adonisjs/core/http'
+import Moment from '#models/moment'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class MomentsController {
-  async store() {
-    return {
-      msg: 'Deu Certo !',
-    }
+  async store({ request, response }: HttpContexContract) {
+    const body = request.body()
+    const moment = await Moment.create(body)
+    response.status(201)
+    return { message: 'Momento criado com sucesso!', data: moment }
   }
 }
